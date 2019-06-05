76c7bc77b58411b409ec17e334a21db89d37cafb
bersiroth <caronbernard1@gmail.com>
2019-06-05 13:38:03 +0200

    add interface context

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,14 +1,14 @@
 // @flow
 import Joi from '@hapi/joi';
 import userSchema from '../schemas/users';
-import type contextType from '../context';
+import type { ContextInterface } from '../interfaces/context';
 
-export default class usersController {
-  static async get(context: contextType) {
+export default class UsersController {
+  static async get(context: ContextInterface) {
     context.sendJson(200, { user: context.getBody() });
   }
 
-  static async create(context: contextType) {
+  static async create(context: ContextInterface) {
     Joi.validate(context.getBody(), userSchema, { abortEarly: false },
       (err: ValidationError) => {
         if (err !== null) {
