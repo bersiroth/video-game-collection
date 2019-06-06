535fc9acd90ce0ebbddd828547df681701141062
bersiroth <caronbernard1@gmail.com>
2019-06-06 13:36:33 +0200

    remove comment

--- src/interfaces/validator.js
+++ src/interfaces/validator.js
@@ -3,6 +3,5 @@ export type validatorError = {message: string};
 export type validatorCallback = (err: ?validatorError, value: ?Object) => void;
 
 export interface ValidatorInterface {
-  // validate: (value: Object, schema: Object, callback: validatorCallback) => void;
   validate(value: Object, schema: Object, callback: validatorCallback): void;
 }
