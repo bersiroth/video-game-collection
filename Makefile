0dc39fa34a4468ad22344e96d14abf39f971981b
bersiroth <caronbernard1@gmail.com>
2019-06-13 16:01:12 +0200

    WIP : Test mocha (#4)
    * init unit test with mocha
    * add circle ci config
    * use config module

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+.PHONY: test
+
 ssh-node:
 	docker-compose exec node bash
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // @flow
 import express from 'express';
+import config from 'config';
 import router from 'tp-node/routes/router';
 
 const app = express();
@@ -7,4 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3000);
+app.listen(config.app.port);
+
+export default app;
